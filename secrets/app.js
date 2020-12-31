ec5fe262b1e19623e524a254db777e51a7573f1f
sarthaksuperb <74700353+sarthaksuperb@users.noreply.github.com>
2020-12-31 12:23:45 +0530

    Add files via upload

---
+++ secrets/app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+require('dotenv').config();
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose = require("mongoose");
+
+const bcrypt=require("bcrypt");
+const app=express();
+const saltRounds=10;
+
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+
+
+const User=new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("secrets");
+      }
+    });
+  });
+
+});
+
+app.post("/login",function(req,res){
+  const username=req.body.username;
+  const password=req.body.password;
+  User.findOne({email:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        bcrypt.compare(password, hash, function(err, result) {
+        if(result===true){
+          res.render("secrets");
+        }
+
+       });
+
+
+      }
+    }
+  });
+});
+
+app.listen(3000,function(){
+  console.log("Server Started on Port 3000");
+});
